014817ffb149b2f47f8cbc4bc4123c25ef9f5d6a
SetnipatKriangsakdchai <>
2018-11-04 23:14:25 +0700

    [Add Card] 1. Fix Detail Information

---
+++ src/components/cardList/addCardList.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text, View , Image , TouchableOpacity} from 'react-native';
+
+export default class addCardList extends Component {
+
+    addCardHandler = () => {  
+        this.props.navigator.push({
+          screen: "TappTact-PageAddCard",
+          title: "Add Card"
+      });
+     }
+
+  render() {
+    return (
+      <View style={styles.containerAddCard}>
+      <TouchableOpacity onPress={this.addCardHandler}>
+        <Text style={styles.addCardText}>Add Card</Text>
+      </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  containerAddCard: {
+    flexGrow: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  addCardText: {
+    fontSize:24,
+    fontWeight:'bold',
+    color:'#1a6aab'
+}
+});
---
+++ src/screens/AddCardScreen/PageAddCard.js
@@ -0,0 +1,26 @@
+
+import React, {Component} from 'react';
+import {StyleSheet, Text, View , ScrollView} from 'react-native';
+import AddCardForm from '../../components/addCard/addCardForm'
+// import RegisterForm from '../../components/register/RegisterForm'
+
+class PageAddCard extends Component {
+    render(){
+        return(
+            <ScrollView>
+                <AddCardForm navigator={this.props.navigator}/>
+            </ScrollView>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+  containerAddCardPage: {
+    flex: 1,
+    paddingTop:20,
+    alignItems: 'center',
+  }
+
+});
+
+export default PageAddCard;
